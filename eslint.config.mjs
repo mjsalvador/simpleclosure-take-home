195cf4769d6ea3531dca64f0a11e50cbd80b5ec2
Matt Salvador <34481831+mjsalvador@users.noreply.github.com>
2026-09-01 10:28:39 -0400

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
+
+export default eslintConfig;
